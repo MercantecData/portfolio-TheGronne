1f1a3a4557d397f68c699455b53a5aa4be09659c
Gronne <kasperg102@gmail.com>
2019-11-04 09:26:12 +0100

    Test

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -6,7 +6,7 @@ namespace Library
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye, World!");
         }
     }
 }
