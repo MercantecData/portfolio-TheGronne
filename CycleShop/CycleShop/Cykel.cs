d324b81dd9352d3e2d053ad1efd0a7f1ba059a65
Gronne <kasperg102@gmail.com>
2019-11-06 10:50:25 +0100

    Shizz isn't working yo

---
+++ CycleShop/CycleShop/Cykel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CycleShop
+{
+    class Cykel
+    {
+        public string manifacturer;
+        public double wheelSize;
+        public string colour;
+        public string gender;
+        public string terrain;
+    }
+}
